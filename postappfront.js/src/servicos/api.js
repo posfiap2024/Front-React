bc9ad504c76d5eafd07f42b6814a5e1272a495e1
RenanPmtl <renangallucci@gmail.com>
2024-09-15 17:05:12 -0300

    Remoção de mock Base URL

--- postappfront.js/src/servicos/api.js
+++ postappfront.js/src/servicos/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3001';
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const obterPosts = async () => {
   try {
