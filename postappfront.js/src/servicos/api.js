65c29607072672cdf9c5fe27cf12fb82634ae5f7
RenanPmtl <renangallucci@gmail.com>
2024-09-22 16:52:05 -0300

    Delete post completo

--- postappfront.js/src/servicos/api.js
+++ postappfront.js/src/servicos/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'http://localhost:3001';
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = 'http://localhost:3001';
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const obterPosts = async () => {
   try {
@@ -83,14 +83,18 @@ export const atualizarPost = async (id, post) => {
   }
 };
 
-export const excluirPost = async (id) => {
+export const excluirPost = async (id, token) => {
   try {
     const response = await fetch(`${BASE_URL}/posts/${id}`, {
       method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
     });
     if (!response.ok) {
       throw new Error('Falha ao excluir post');
     }
+    console.log('Post excluído com sucesso!');
     return true;
   } catch (error) {
     console.error('Erro ao excluir post:', error);
